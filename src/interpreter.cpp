f93fb0e27b6098339db3289716a8b6c82c002f70
robosa-Surface <robosa-Surface@example.com>
2023-05-31 16:53:44 +0900

    add functionality to publish cmd_shooting_velocity

--- src/interpreter.cpp
+++ src/interpreter.cpp
@@ -5,6 +5,7 @@
 
 ros::Publisher cmdAnglePub;
 ros::Publisher cmdShootingDutyPub;
+ros::Publisher cmdShootingVelocityPub;
 
 ros::Subscriber cmdAimingPoleSub;
 ros::Subscriber cmdReceiveSub;
@@ -24,7 +25,8 @@ constexpr double degToRad(int deg)
 {
     return deg * 3.1415926535 / 180;
 }
-int poleTargetDuties[] = {470, 370, 470, 530, 530, 640, 620, 620, 620, 620, 620};
+int poleTargetDuties[11] = {};
+double poleTargetVelocities[11] = {};
 double poleTargetAngles[] = {degToRad(-45), degToRad(0), degToRad(45), degToRad(-30), degToRad(30), degToRad(0), degToRad(-15), degToRad(15), degToRad(-20), degToRad(0), degToRad(20)};
 
 
@@ -36,8 +38,11 @@ void aimingPoleCb(const std_msgs::Int16 &polemsg)
     angle_pub.data = poleTargetAngles[aimingPole];
     std_msgs::Int16 duty_pub;
     duty_pub.data = poleTargetDuties[aimingPole];
+    std_msgs::Float64 velocity_pub;
+    velocity_pub.data = poleTargetVelocities[aimingPole];
     cmdAnglePub.publish(angle_pub);
     cmdShootingDutyPub.publish(duty_pub);
+    cmdShootingVelocityPub.publish(velocity_pub);
 }
 
 void receiveCb(const std_msgs::Bool &receiveMsg)
@@ -73,12 +78,16 @@ int main(int argc, char **argv)
 
     cmdAnglePub = nh.advertise<std_msgs::Float64>("cmd_angle", 10);
     cmdShootingDutyPub = nh.advertise<std_msgs::Int16>("cmd_shooting_duty", 10);
+    cmdShootingVelocityPub = nh.advertise<std_msgs::Float64>("cmd_shooting_velocity", 10);
 
     cmdAimingPoleSub = nh.subscribe("cmd_aiming_pole", 10, aimingPoleCb);
     cmdReceiveSub = nh.subscribe("cmd_receive", 10, receiveCb);
 
     XmlRpc::XmlRpcValue pole_duty;
+    XmlRpc::XmlRpcValue pole_velocity;
     nh.getParam("/pole/duty", pole_duty);
+    nh.getParam("/pole/velocity", pole_velocity);
+
     
     /*To ensure the reading will happen if the data is provided in right format*/
     if (pole_duty.getType() == XmlRpc::XmlRpcValue::TypeArray)
@@ -86,6 +95,14 @@ int main(int argc, char **argv)
         for (int i = 0; i < pole_duty.size(); i++)
         {
             poleTargetDuties[i] = pole_duty[i];
+        }
+    }
+    if (pole_velocity.getType() == XmlRpc::XmlRpcValue::TypeArray)
+    {
+        for (int i = 0; i < pole_velocity.size(); i++)
+        {
+            poleTargetVelocities[i] = pole_velocity[i];
+            ROS_INFO("pole vel %lf", poleTargetVelocities[i]);
         }
     }
 
